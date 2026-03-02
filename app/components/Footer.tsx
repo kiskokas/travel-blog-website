01195088d7b4da5bf6dd615c7f027dd1351455f0
Kriszitan Nagy -X (krnagy - IQOR GLOBAL SERVICES LLC at Cisco) <krnagy@cisco.com>
2026-03-02 16:23:01 -0500

    footer bg change

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black text-white/150 uppercase text-[10px] text-center p-4 flex justify-center items-center gap-4">
+    <footer className="bg-[#0a0a0a] text-white/150 uppercase text-[10px] text-center p-4 flex justify-center items-center gap-4">
       <span>© {new Date().getFullYear()} Travel blog</span>
     </footer>
   );
